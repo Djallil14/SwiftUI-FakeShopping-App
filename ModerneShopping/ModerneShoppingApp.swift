56b91ae157fa6f60adc7dea40cbfa5796f91e5f4
Djallil Elkebir <elkebir.djallil@icloud.com>
2021-09-01 17:34:53 -0400

    Initial Commit

---
+++ ModerneShopping/ModerneShoppingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModerneShoppingApp.swift
+//  ModerneShopping
+//
+//  Created by Djallil Elkebir on 2021-09-01.
+//
+
+import SwiftUI
+
+@main
+struct ModerneShoppingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
